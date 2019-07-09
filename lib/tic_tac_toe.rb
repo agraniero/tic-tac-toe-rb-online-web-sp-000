af432dd48023199163075163d09a43a2d234e2c6
Anthony Graniero <ananfro@gmail.com>
2019-07-09 08:32:59 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -32,6 +32,24 @@ def turn(board)
   end
 end
 
+def current_player(board)
+  if turn_count(board) % 2 == 0
+    return "X"
+  else
+    return "O"
+  end
+end
+
+def turn_count(board)
+  counter = 0
+  board.each do |spot|
+    if spot == 'X' || spot == "O"
+      counter += 1
+    end
+  end
+  counter
+end
+
 
 # Helper Method
 def position_taken?(board, index)
